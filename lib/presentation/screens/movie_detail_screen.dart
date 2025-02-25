f511a498d22e8b5bba1f4d7442feb44c7be05502
franmc01 <francmarinc@gmail.com>
2025-02-24 22:04:49 -0500

    feat: add detail movie page

---
+++ lib/presentation/screens/movie_detail_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MovieDetailScreen extends StatelessWidget {
+  static const name = 'movie_detail_screen';
+
+  final String movieId;
+
+  const MovieDetailScreen({super.key, required this.movieId});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text('MovieID: $movieId'),
+      ),
+    );
+  }
+}
--- lib/shared/router/app_router.dart
+++ lib/shared/router/app_router.dart
@@ -1,13 +1,19 @@
 import 'package:app/presentation/screens/home_screen.dart';
+import 'package:app/presentation/screens/movie_detail_screen.dart';
 import 'package:go_router/go_router.dart';
 
-final appRouter = GoRouter(
-  initialLocation: '/',
-  routes: [
-    GoRoute(
+final appRouter = GoRouter(initialLocation: '/', routes: [
+  GoRoute(
       path: '/',
       name: HomeScreen.name,
       builder: (context, state) => HomeScreen(),
-    )
-  ]
-);
+      routes: [
+        GoRoute(
+          path: 'movie/:id',
+          name: MovieDetailScreen.name,
+          builder: (context, state) => MovieDetailScreen(
+            movieId: state.pathParameters['id'] ?? 'no-id'
+          ),
+        )
+      ]),
+]);
